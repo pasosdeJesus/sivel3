fb3ae2a45bd56927c6c90c0c1ebdd292ee313984
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-02-24 10:52:53 -0500

    advance deploy

--- apps/hardhat/bin/prepadJ.sh
+++ apps/hardhat/bin/prepadJ.sh
@@ -5,6 +5,7 @@ mkdir -p tmpart
 sed -e "s/freebsd/openbsd/g" node_modules/@nomicfoundation/solidity-analyzer/index.js > tmpart/index-sa.js
 cp tmpart/index-sa.js node_modules/@nomicfoundation/solidity-analyzer/index.js
 cd node_modules/@nomicfoundation/solidity-analyzer/
+doas corepack enable
 yarn 
 yarn build
 cd ../../..
