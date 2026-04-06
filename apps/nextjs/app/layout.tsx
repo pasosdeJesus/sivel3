1d080707bcb07668d0c4ee08bfcb4c0f7cf72128
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-04-06 06:28:00 -0500

    talent id

--- apps/nextjs/app/layout.tsx
+++ apps/nextjs/app/layout.tsx
@@ -22,6 +22,9 @@ export default async function RootLayout({
     ["en", "es"].includes(locale) ? locale : "en"
   return (
     <html lang={lang}>
+      <head>
+        <meta name="talentapp:project_verification" content="f6b919387e34e3d13e8d6569e6c9899cedb623bd1d4a901885f2fd4f43e60df63214ab0b2451f93892729bffba84fec96cec9555461a6fb625e4c30f72020cda"/>
+      </head>
       <body className={inter.className}>
         {children}
       </body>
