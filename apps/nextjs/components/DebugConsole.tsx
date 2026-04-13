c6e8413101af6e9a6a63226a6b8cbe245405d56e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-04-13 12:39:52 -0400

    use. Advacing #23

--- apps/nextjs/components/DebugConsole.tsx
+++ apps/nextjs/components/DebugConsole.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useLogger } from '@/lib/logger'
 import { Terminal, X, ChevronUp, ChevronDown, Trash2 } from 'lucide-react'
 
