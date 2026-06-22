4a7dfdbffe31a969feeb55ffcda41d982a2beace
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2026-06-22 13:49:43 -0400

    check verif

--- apps/nextjs/bin/check-verification.mjs
+++ apps/nextjs/bin/check-verification.mjs
@@ -14,7 +14,7 @@ import { resolve, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-config({ path: resolve(__dirname, '..', '.env') })
+config({ path: resolve(__dirname, '..', '..', '.env') })
 
 const wallet = process.argv[2]
 if (!wallet) {
